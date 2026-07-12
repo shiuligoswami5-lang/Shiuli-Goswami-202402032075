163a00d1a62dc047de6382a44b8ff55c48907c8a
shiuligoswami5-lang <shiuligoswami5@gmail.com>
2026-07-12 16:38:29 +0000

    sg4

---
+++ sg4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, f;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    f = n + 5;
+
+    printf("f(n) = %d\n", f);
+
+    return 0;
+}
